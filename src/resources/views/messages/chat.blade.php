99472ae91aa397626c38365db16b2c4bd4c25a4e
nagamoridaiki <no.swea1017@gmail.com>
2020-05-22 00:31:48 +0900

    メッセージ一覧のUI/UXの改修

--- src/resources/views/messages/chat.blade.php
+++ src/resources/views/messages/chat.blade.php
@@ -1,67 +1,119 @@
 @extends('app')
 
-@section('title', $friend->name . 'さんとのチャット')
+@section('title', 'メッセージ一覧')
+
 
 @section('content')
-  @include('nav')
-  <div class="container">
+@include('nav')
 
-  @foreach($messages as $key => $message)
-    {{--   自分が送信したメッセージ  右側--}}
-      @if($message->send_user_id == $user->id && $message->receive_user_id == $friend->id)
-          <div class="send" style="text-align:right">
-              <div class="my_info">
-                  <div class="my_info_name">
-                      <p>{{$user->name}}</p>
-                  </div>
-                  <div class="my_info_photo">
-                      @if(!empty($user->image))
-                              <img class='prof-photo' src="{{ asset('storage/images/'.$user->image) }}" >
-                      @else
-                              <i class="fas fa-user-circle fa-3x mr-1"></i>
-                      @endif
-                  </div>
-              </div>
-              <div class="my_speek">
-                  <p>{{$message->message_text}}</p>
-              </div>
-              <p>{{$message->created_at}}</p>
-          </div>
-      @endif
+メッセージ一覧
+<div class="card grey lighten-3 chat-room">
+  <div class="card-body">
+    <!-- Grid row -->
+    <div class="row px-lg-2 px-2">
+      <!-- Grid column -->
+      <div class="col-md-4 col-xl-4 px-0">
+        <div class="white z-depth-1 px-3 pt-3 pb-0">
+        @foreach($message_array as $key => $message)
+          <ul class="list-unstyled friend-list">
+            <li class="active grey lighten-3 p-2">
+                <a href="{{ route('message', [ 'name' => $userall[$user_array[$key]-1]->name ]) }}" class="text-muted">
+                    @if(!empty($userall[$user_array[$key]-1]->image))
+                            <img class='prof-photo' src="{{ asset('storage/images/'.$userall[$user_array[$key]-1]->image) }}" >
+                    @else
+                            <i class="fas fa-user-circle fa-3x mr-1"></i>
+                    @endif
+                    <div class="text-small">
+                        <strong>{{ $userall[$user_array[$key]-1]->name }}</strong>
+                        <p class="last-message text-muted">{{ $messageall[$message -1]->message_text }}</p>
+                    </div>
+                    <div class="chat-footer">
+                        <p class="text-smaller text-muted mb-0">{{ $messageall[$message -1]->created_at }}</p>
+                    </div>
+                </a>
+              </a>
+            </li>
+          </ul>
+        @endforeach
+        </div>
+      </div>
+      <!-- Grid column -->
 
-      {{--   相手が送信したメッセージ  左側--}}
-      @if($message->send_user_id == $friend->id && $message->receive_user_id == $user->id)
-          <div class="recieve" style="text-align: left">
-              <div class="your_info">
-                  <div class="your_info_name">
-                      <p>{{$friend->name}}</p>
-                  </div>
-                  <div class="your_info_photo">
-                      @if(!empty($friend->image))
-                              <img class='prof-photo' src="{{ asset('storage/images/'.$friend->image) }}" >
-                      @else
-                              <i class="fas fa-user-circle fa-3x mr-1"></i>
-                      @endif
-                  </div>
-              </div>
-              <div class="your_speek">
-                  <p>{{$message->message_text}}</p>
-              </div>
-              <p>{{$message->created_at}}</p>
-          </div>
-      @endif
-  @endforeach
+      <!-- Grid column -->
+      <div class="col-md-6 col-xl-8 pl-md-3 px-lg-auto px-0">
+      <h5>{{$friend->name}}さんとのチャット</h5>
+        <div class="chat-message">
+        <ul class="list-unstyled chat">
+            @foreach($messages as $key => $message)
+                {{--   相手が送信したメッセージ  左側--}}
+                @if($message->send_user_id == $friend->id && $message->receive_user_id == $user->id)
+                    <li class="d-flex mb-4">
+                    <div class="your_info_photo">
+                        @if(!empty($friend->image))
+                                <img class='prof-photo' src="{{ asset('storage/images/'.$friend->image) }}" >
+                        @else
+                                <i class="fas fa-user-circle fa-3x mr-1"></i>
+                        @endif
+                    </div>
+                    <div class="chat-body white p-3 ml-2 z-depth-1">
+                        <div class="header">
+                            <strong class="primary-font">{{$friend->name}}</strong>
+                            <small class="text-muted"><i class="far fa-clock"></i> {{$message->created_at}}</small>
+                        </div>
+                        <hr class="w-100">
+                        <p class="mb-0">
+                            {{$message->message_text}}
+                        </p>
+                    </div>
+                    </li>
+                @endif
 
-  <form method="POST" action="{{ route('message.send', ['name' => $friend->name] ) }}">
-    @csrf
-    <div class="md-form">
-      <label>メッセージ</label>
-      <input type="text" name="message_text" class="form-control">
-      <input type="hidden" name="send_user_id" value="{{$param['send']??''}}">
-      <input type="hidden" name="receive_user_id" value="{{$param['recieve']??''}}">
+                {{--   自分が送信したメッセージ  右側--}}
+                @if($message->send_user_id == $user->id && $message->receive_user_id == $friend->id)
+                    <li class="d-flex flex-row-reverse mb-4">
+                        <div class="my_info_photo">
+                            @if(!empty($user->image))
+                                    <img class='prof-photo' src="{{ asset('storage/images/'.$user->image) }}" >
+                            @else
+                                    <i class="fas fa-user-circle fa-3x mr-1"></i>
+                            @endif
+                        </div>
+
+                        <div class="chat-body white p-3 z-depth-1" >
+                            <div class="header">
+                                <strong class="primary-font">{{$user->name}}</strong>
+                                <small class="pull-right text-muted">
+                                    <i class="far fa-clock"></i>{{$message->created_at}}
+                                </small>
+                            </div>
+                            <hr class="w-100">
+                            <p class="mb-0">
+                                {{$message->message_text}}
+                            </p>
+                        </div>
+                    </li>
+                @endif
+            @endforeach
+            <form method="POST" action="{{ route('message.send', ['name' => $friend->name] ) }}">
+                @csrf
+                <li class="white">
+                    <div class="form-group basic-textarea">
+                        <textarea class="form-control pl-2 my-0" id="exampleFormControlTextarea2" rows="3" placeholder="メッセージを入力"></textarea>
+                    </div>
+                </li>
+                <input type="hidden" name="send_user_id" value="{{$param['send']??''}}">
+                <input type="hidden" name="receive_user_id" value="{{$param['recieve']??''}}">
+                <button type="submit" class="btn blue-gradient btn-block">送信する</button>
+            </form>
+        </ul>
+        </div>
+      </div>
+      <!-- Grid column -->
+      
     </div>
-    <button type="submit" class="btn blue-gradient btn-block">送信する</button>
-  </form>
-
+    <!-- Grid row -->
   </div>
+</div>
 @endsection
+
+   
--- src/resources/views/messages/message_list.blade.php
+++ src/resources/views/messages/message_list.blade.php
@@ -7,25 +7,103 @@
 @include('nav')
 
 メッセージ一覧
-
-@foreach($message_array as $key => $message)
-<div class="container">
-    <div class="card mt-3 ">
-        <div class="col-md-8 col-xs-12">
-        <a href="{{ route('message', [ 'name' => $userall[$user_array[$key]-1]->name ]) }}" class="text-muted">
-            @if(!empty($userall[$user_array[$key]-1]->image))
-                    <img class='prof-photo' src="{{ asset('storage/images/'.$userall[$user_array[$key]-1]->image) }}" >
-            @else
-                    <i class="fas fa-user-circle fa-3x mr-1"></i>
-            @endif
-            {{ $userall[$user_array[$key]-1]->name }}
-            {{ $messageall[$message -1]->message_text }}
-        </a>
+<div class="card grey lighten-3 chat-room">
+  <div class="card-body">
+    <!-- Grid row -->
+    <div class="row px-lg-2 px-2">
+      <!-- Grid column -->
+      <div class="col-md-4 col-xl-4 px-0">
+        <div class="white z-depth-1 px-3 pt-3 pb-0">
+        @foreach($message_array as $key => $message)
+          <ul class="list-unstyled friend-list">
+            <li class="active grey lighten-3 p-2">
+                <a href="{{ route('message', [ 'name' => $userall[$user_array[$key]-1]->name ]) }}" class="text-muted">
+                    @if(!empty($userall[$user_array[$key]-1]->image))
+                            <img class='prof-photo' src="{{ asset('storage/images/'.$userall[$user_array[$key]-1]->image) }}" >
+                    @else
+                            <i class="fas fa-user-circle fa-3x mr-1"></i>
+                    @endif
+                    <div class="text-small">
+                        <strong>{{ $userall[$user_array[$key]-1]->name }}</strong>
+                        <p class="last-message text-muted">{{ $messageall[$message -1]->message_text }}</p>
+                    </div>
+                    <div class="chat-footer">
+                        <p class="text-smaller text-muted mb-0">{{ $messageall[$message -1]->created_at }}</p>
+                    </div>
+                </a>
+              </a>
+            </li>
+          </ul>
+        @endforeach
         </div>
-    </div>
-</div>
-@endforeach
+      </div>
+      <!-- Grid column -->
 
+      <!-- Grid column -->
+      <div class="col-md-6 col-xl-8 pl-md-3 px-lg-auto px-0">
+
+        <div class="chat-message">
+
+          <ul class="list-unstyled chat">
+            <li class="d-flex justify-content-between mb-4">
+              <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-6.jpg" alt="avatar" class="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1">
+              <div class="chat-body white p-3 ml-2 z-depth-1">
+                <div class="header">
+                  <strong class="primary-font">Brad Pitt</strong>
+                  <small class="pull-right text-muted"><i class="far fa-clock"></i> 12 mins ago</small>
+                </div>
+                <hr class="w-100">
+                <p class="mb-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                  labore et dolore magna aliqua.
+                </p>
+              </div>
+            </li>
+            <li class="d-flex justify-content-between mb-4">
+              <div class="chat-body white p-3 z-depth-1">
+                <div class="header">
+                  <strong class="primary-font">Lara Croft</strong>
+                  <small class="pull-right text-muted"><i class="far fa-clock"></i> 13 mins ago</small>
+                </div>
+                <hr class="w-100">
+                <p class="mb-0">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                  laudantium.
+                </p>
+              </div>
+              <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg" alt="avatar" class="avatar rounded-circle mr-0 ml-3 z-depth-1">
+            </li>
+            <li class="d-flex justify-content-between mb-4 pb-3">
+              <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-6.jpg" alt="avatar" class="avatar rounded-circle mr-2 ml-lg-3 ml-0 z-depth-1">
+              <div class="chat-body white p-3 ml-2 z-depth-1">
+                <div class="header">
+                  <strong class="primary-font">Brad Pitt</strong>
+                  <small class="pull-right text-muted"><i class="far fa-clock"></i> 12 mins ago</small>
+                </div>
+                <hr class="w-100">
+                <p class="mb-0">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                  labore et dolore magna aliqua.
+                </p>
+              </div>
+            </li>
+            <li class="white">
+              <div class="form-group basic-textarea">
+                <textarea class="form-control pl-2 my-0" id="exampleFormControlTextarea2" rows="3" placeholder="Type your message here..."></textarea>
+              </div>
+            </li>
+            <button type="button" class="btn btn-info btn-rounded btn-sm waves-effect waves-light float-right">Send</button>
+          </ul>
+
+        </div>
+
+      </div>
+      <!-- Grid column -->
+      
+    </div>
+    <!-- Grid row -->
+  </div>
+</div>
 @endsection
 
    
